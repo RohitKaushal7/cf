fab83ff15c96de7574e43ad0e5afa23a86fdf557
RohitKaushal7 <rohitkk252550@gmail.com>
2020-03-24 20:11:04 +0530

    little responsive

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./Card.scss";
 
 const Card = props => {
+  let dir = props.dir;
+  if (window.innerWidth < 1000) {
+    if (props.res) {
+      dir = false;
+    }
+  }
   return (
     <div
       className={`card ${props.res ? "res" : ""}`}
@@ -12,7 +18,7 @@ const Card = props => {
         background: props.bg || "#fff9",
         borderRadius: "1em",
         display: "inline-flex",
-        flexDirection: props.dir || "column",
+        flexDirection: dir || "column",
         padding: props.p || "0em 0",
         margin: props.m || "0em",
         boxShadow: `0 0 ${props.sh} #0003`,
